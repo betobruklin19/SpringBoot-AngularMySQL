eac10ee35752e035a96167abcf7ffa58a19d9da2
betobruklin19 <robertosilva1991@outlook.com>
2024-06-15 14:56:50 -0300

    criação das ações do CRUD no controller da aplicação

--- api/src/main/java/br/com/projeto/api/controller/Controller.java
+++ api/src/main/java/br/com/projeto/api/controller/Controller.java
@@ -1,14 +1,44 @@
 package br.com.projeto.api.controller;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RestController;
 
+import br.com.projeto.api.model.Cliente;
+import br.com.projeto.api.repository.Repository;
+
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+
 @RestController
+@CrossOrigin(origins = "*")
 public class Controller {
 
+    @Autowired
+    private Repository repository;
+
+    @PostMapping("/")
+    public Cliente cadastrar(@RequestBody Cliente cliente) {
+        return repository.save(cliente);
+    }
+
     @GetMapping("/")
-    public String teste() {
-        return "Hello World - Spring + Angular!";
+    public Iterable<Cliente> selecionar() {
+        return repository.findAll();
+    }
+
+    @PutMapping("/")
+    public Cliente editar(@RequestBody Cliente cliente) {
+        return repository.save(cliente);
+    }
+
+    @DeleteMapping("/{codigo}")
+    public void remover(@PathVariable long codigo) {
+        repository.deleteById(codigo);
     }
 
 }
--- api/src/main/java/br/com/projeto/api/repository/Repository.java
+++ api/src/main/java/br/com/projeto/api/repository/Repository.java
@@ -1,5 +1,9 @@
 package br.com.projeto.api.repository;
 
-public interface Repository {
+import org.springframework.data.repository.CrudRepository;
+import br.com.projeto.api.model.Cliente;
+
+// @org.springframework.stereotype.Repository
+public interface Repository extends CrudRepository<Cliente, Long> {
 
 }
